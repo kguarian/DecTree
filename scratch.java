3b64bc831cfa6e0e71aa7f08c252d54ede6abd41
uTwitch1001 <uTwitch1001@gmail.com>
2020-07-29 15:19:36 -0700

    tinkerfiles

--- scratch.java
+++ scratch.java
@@ -1,7 +1,7 @@
 public class scratch {
     Number x = 0;
 
-    public void main(String[] args) {
-        x = (long) x;
+    public static void main(String[] args) {
+        System.out.println(1d/5693);
     }
 }
